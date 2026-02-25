e5050bb86f08d0a6c526cae9f120dfed4be023a1
Raphael Levecque <raphael.levecque.etu@univ-lille.fr>
2026-02-25 14:11:19 +0100

    fix unused quantity parameter

--- fox-paradox/src/scripts/password_generator.gd
+++ fox-paradox/src/scripts/password_generator.gd
@@ -8,7 +8,7 @@ func generate_passes(word: String = "", quantity: int = 5):
 	if word:
 		seed = hash(word)
 	rng.seed = seed
-	for i in range(0, 5):
+	for i in range(0, quantity):
 		var l = []
 		for j in range(0,4):
 			l.append(rng.randi_range(0,5))
