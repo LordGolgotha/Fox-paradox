278b511a83b72ad1d6f1527082434a0d4e568d47
Raphael Levecque <raphael.levecque.etu@univ-lille.fr>
2026-02-25 16:54:04 +0100

    add level switching and pause menu

--- fox-paradox/src/util/levels_controler.gd
+++ fox-paradox/src/util/levels_controler.gd
@@ -1,5 +1,6 @@
 extends Node
 
 var level_list={
-	"test_level":"res://src/scenes/level/TestLevel.tscn"
+	"test_level":"res://src/scenes/level/TestLevel.tscn",
+	"test_level2":"res://src/scenes/level/TestLevel2.tscn",
 }
