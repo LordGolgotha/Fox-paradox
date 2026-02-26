c0c0b30e5962094aed8f78d1769af211d9bf95ba
Dipre <clement.dipre.etu@univ-lille.fr>
2026-02-26 09:46:02 +0100

    feat: added fox animation

--- fox-paradox/src/scripts/entity/fox.gd
+++ fox-paradox/src/scripts/entity/fox.gd
@@ -16,10 +16,15 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	walk()
+	move_and_slide()
+
+func walk():
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
+		$AnimatedSprite2D.flip_h = (direction == -1)
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		$AnimatedSprite2D.play("idle")
