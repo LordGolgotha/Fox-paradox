3c56081ad47e0776b0e9c9d4ee32c89667027da3
LordGolgotha <ugo.barth.etu@univ-lille.fr>
2026-02-27 15:08:49 +0100

    Niveau lac puits fini

--- fox-paradox/src/util/levels_controler.gd
+++ fox-paradox/src/util/levels_controler.gd
@@ -3,5 +3,6 @@ extends Node
 var level_list={
 	"tuto_summer":"res://src/scenes/levels/summer/tuto_summer.tscn",
 	"tuto_winter":"res://src/scenes/levels/winter/tuto_winter.tscn",
-	"winter_lake_well":"res//src/scenes/levels/winter/winter_lake_well.tscn",
+	"summer_lake_well":"res://src/scenes/levels/summer/summer_lake_well.tscn",
+	"deep_well":"res://src/scenes/levels/summer/deep_well.tscn",
 }
