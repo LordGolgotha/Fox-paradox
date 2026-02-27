96dc83bb4d1e4238499b356a067911850318f7a7
Dipre <clement.dipre.etu@univ-lille.fr>
2026-02-27 16:08:07 +0100

    fix fox, ajout bruit cerf

--- fox-paradox/src/scripts/entity/interactable/cerf_cascade_summer.gd
+++ fox-paradox/src/scripts/entity/interactable/cerf_cascade_summer.gd
@@ -9,6 +9,7 @@ func _process(delta):
 	if inside:
 		if Input.is_action_just_pressed("Interact"):
 			$"../glyphes".visible = true
+			$AudioStreamPlayer.play()
 	
 func enter(body:Node2D):
 	if body is Fox:
