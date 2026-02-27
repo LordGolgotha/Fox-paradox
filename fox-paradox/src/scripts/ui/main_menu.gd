724d7f036e4ed59121ca5d5082cfaedb4b2f3beb
Napstio <NapstioTheRobot@outlook.fr>
2026-02-27 16:51:38 +0100

    Gamepad for Title Screen

--- fox-paradox/src/scripts/ui/main_menu.gd
+++ fox-paradox/src/scripts/ui/main_menu.gd
@@ -4,6 +4,9 @@ extends Node
 @onready var summer_winter_vbox : VBoxContainer = $SummerWinterVBox
 @onready var seed_line_edit : LineEdit = $SeedVBox/SeedLineEdit
 @onready var error_label : Label = $SeedVBox/ErrorLabel
+@onready var quit_button: Button = $QuitHBox/QuitButton
+@onready var summer_button: Button = $SummerWinterVBox/HBoxContainer/SummerButton
+
 
 func _on_summer_button_button_down() -> void:
 	GameManager.go_to_level("tuto_summer")
@@ -24,7 +27,15 @@ func _on_confirm_seed_button_button_down() -> void:
 		error_label.text = ""
 		seed_vbox.visible = false
 		summer_winter_vbox.visible = true
+		summer_button.grab_focus.call_deferred()
+		
 		
 func _on_back_button_button_down() -> void:
 	seed_vbox.visible = true
 	summer_winter_vbox.visible = false
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Cancel") and quit_button.visible == true:
+		_on_quit_button_button_down()
+	if Input.is_action_just_pressed("pause") and seed_vbox.visible == true:
+		_on_confirm_seed_button_button_down()
