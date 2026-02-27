b545539cce4f437c3fa85dd271fd915859737ef8
Napstio <NapstioTheRobot@outlook.fr>
2026-02-27 13:30:01 +0100

    Gamepad Support

--- fox-paradox/src/scripts/entity/interactable.gd
+++ fox-paradox/src/scripts/entity/interactable.gd
@@ -3,8 +3,9 @@ class_name Interactable
 
 @onready var notification: Sprite2D = $Notification
 
-enum State{IN, OUT, DONE}
+enum State{IN, OUT, DONE, PAUSE}
 var current_state = State.OUT
+var old_state = State.OUT
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,10 +24,17 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func _on_body_exited(body: Node2D) -> void:
-	if current_state != State.DONE:
+	if current_state != State.DONE and current_state != State.PAUSE:
 		notification.visible = false
 		current_state = State.OUT
 	
 func finish():
 	notification.visible = false
 	current_state = State.DONE
+	
+func pause():
+	old_state = current_state
+	current_state = State.PAUSE
+
+func unpause():
+	current_state = old_state
